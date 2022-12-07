a197f7d87a127989cfda8c42396d82aca8bab1f0
Bhuvana-Ravikumar <ravikumar.bh@northeastern.edu>
2022-12-06 23:08:13 -0500

    IA role

---
+++ src/Business/Role/IARole.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.En;
+import Business.Organization.IAOrganization;
+import Business.Organization.Org;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.InsuranceAgentRole.JAWAJpanel;
+
+/**
+ *
+ * @author bhuvana
+ */
+public class IARole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Org organization, En enterprise, EcoSystem business) {
+     return new JAWAJpanel(userProcessContainer, account, (IAOrganization)organization, enterprise);
+    }
+    
+}
